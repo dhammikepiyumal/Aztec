45d3d2feb4ab39ecde059c78a1fca22405ebbf52
dhammikepiyumal <dhammikepiyumal@gmail.com>
2020-06-24 23:14:11 +0530

    E19

--- Aztec/src/com/dhammikepiyumal/aztec/graphics/Screen.java
+++ Aztec/src/com/dhammikepiyumal/aztec/graphics/Screen.java
@@ -6,7 +6,7 @@ public class Screen {
     private int width, height;
     private Random random = new Random();
     public int[] pixels;
-    public final int MAP_SIZE = 8;
+    public final int MAP_SIZE = 64;
     public final int MAP_SIZE_MASK = MAP_SIZE - 1;
     public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 
